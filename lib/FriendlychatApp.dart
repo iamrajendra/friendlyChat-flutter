15df4631b4025e94e80f574120e959a9dabf102e
Rajendra Verma <gamblerindore@gmail.com>
2019-12-19 10:52:43 +0530

    convert kIOSTheme to private

--- lib/FriendlychatApp.dart
+++ lib/FriendlychatApp.dart
@@ -5,13 +5,13 @@ import 'package:flutter/material.dart';
 import 'ChatScreen.dart';
 
 
-final ThemeData kIOSTheme = new ThemeData(
+final ThemeData _kIOSTheme = new ThemeData(
   primarySwatch: Colors.orange,
   primaryColor: Colors.grey[100],
   primaryColorBrightness: Brightness.light,
 );
 
-final ThemeData kDefaultTheme = new ThemeData(
+final ThemeData _kDefaultTheme = new ThemeData(
   primarySwatch: Colors.purple,
   accentColor: Colors.orangeAccent[400],
 );
@@ -20,7 +20,7 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
-      theme: kDefaultTheme,
+      theme: _kDefaultTheme,
 
       home: new ChatScreen(),
     );
